145f1f46a3c4240add53ecc31c6fdef539d80849
Matts2011 <adriancho2011@icloud.com>
2026-07-30 19:22:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,49 @@ const fotos = [
 ];
 
 let numero = 0;
+const imagen = document.getElementById("fondo");
 
-function cambiarFoto() {
-    numero++;
+function cambiarFoto(){
 
-    if (numero >= fotos.length) {
-        numero = 0;
-    }
+    imagen.style.opacity = 0;
 
-    document.getElementById("fondo").src = fotos[numero];
+    setTimeout(() => {
+
+        imagen.src = fotos[numero];
+
+        imagen.onload = () => {
+            imagen.style.opacity = 1;
+        };
+
+        numero++;
+
+        if(numero >= fotos.length){
+            numero = 0;
+        }
+
+    },1000);
 }
 
-setInterval(cambiarFoto, 10000);
+cambiarFoto();
+setInterval(cambiarFoto,14000);
+
 
 function actualizarHora(){
+
     let ahora = new Date();
 
     document.getElementById("hora").innerHTML =
-    ahora.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit"
+    ahora.toLocaleTimeString("es-ES",{
+        hour:"2-digit",
+        minute:"2-digit",
+        hour12:false
     });
 
     document.getElementById("fecha").innerHTML =
-    ahora.toLocaleDateString("es-ES", {
-        weekday: "short",
-        day: "numeric",
-        month: "short"
+    ahora.toLocaleDateString("es-ES",{
+        weekday:"long",
+        day:"numeric",
+        month:"long"
     });
 }
 
